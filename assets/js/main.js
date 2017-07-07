90e496801894a7bdf7e8b2fe93fa4c47ce934a61
maribel <maribel.diaz.j@gmail.com>
2017-07-07 16:08:20 -0400

    visualizando img

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,30 @@
-$(document).ready(function($){
 
-/*llamanndo a todos los pokemon*/
-	function consumiendoApi(url){
+$(document).ready(function() {
+    $.ajax({
+        url: 'https://pokeapi.co/api/v2/pokemon/',
+        type: 'GET',
+        dataType: 'JSON',
+        data: {"limit": '811'},
+    })
+    .done(function(result) {
+        console.log("success");
+        console.log(result);
+        for (var i = 1; i < 719; i++) {
+            var poke = $("<img src=https://pokeapi.co/media/img/" + i + ".png id=" + i + ">");
+            $('div.pokemon').append(poke);
+        }
+    })
+    .fail(function() {
+        console.log("error");
+    })
+    .always(function() {
+        console.log("complete");
+    })
+});
+
+
+/*llamando a todos los pokemon*/
+/*	function consumiendoApi(url){
 		$.ajax({
    	url: 'http://pokeapi.co/api/v2/pokemon/?limit=811',
    	type: 'GET',
@@ -11,7 +34,7 @@ $(document).ready(function($){
    .done(function() {
    	console.log("success");
    	for(x in success.data){
-   		$('#pokemon').append('<li><img src="'+success.data[x].image.thubnail.url+'"></li');
+   		$('#pokemon').append('<li><img src="'+success.data[x].png.thumbnail.url+'"></li');
    	}
    })
    .fail(function(){
@@ -22,10 +45,7 @@ $(document).ready(function($){
    });
    };
 
-/*ajax para mostrar el modal*/
-    function llamandoModal(modal){
-     $('.modal-trigger').leanModal();
+*/
 
-    }
 
-});
+
